0d995a97fecaad143d10c302d2bcff19d10310d6
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-06-11 00:33:17 -0700

    changes

--- server/client/src/components/SCWidget.js
+++ server/client/src/components/SCWidget.js
@@ -6,7 +6,7 @@ class SCWidget extends React.Component {
 
     this.state = {
       src:
-        'https://w.soundcloud.com/player/?url=http://api.soundcloud.com/users/1539950/favorites',
+        'https://open.spotify.com/embed?uri=spotify:user:invictusforever:playlist:2lW5hDsiy4vJj0AOfZlh9R',
       queue: []
     };
 
@@ -19,23 +19,14 @@ class SCWidget extends React.Component {
     return (
       <div>
         <iframe
-          title="SCWidget"
-          id="sc-widget"
           src={this.state.src}
-          width="100%"
-          height="465"
-          scrolling="no"
-          frameBorder="no"
-        />
-
-        <iframe
-          src="https://open.spotify.com/embed?uri=spotify:user:invictusforever:playlist:2lW5hDsiy4vJj0AOfZlh9R"
           width="300"
           height="380"
           frameborder="0"
           allowtransparency="true"
           allow="encrypted-media"
         />
+        <hr />
         <button onClick={() => this.funTimes()}>Surprise me</button>
       </div>
     );
