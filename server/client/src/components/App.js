c8cbcd4fd12cc7918eb9f5106ded8a9be1171ed4
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-06-18 16:21:41 -0700

    minor changes

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './Header';
-import Chat from './Chat';
-import SCWidget from './SCWidget';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import Chat from "./Chat";
+import SCWidget from "./SCWidget";
 
 class App extends Component {
   render() {
