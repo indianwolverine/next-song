b4c4968fccbc79a08fb91a4ac171dd66bb5ad093
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-07-08 22:32:31 -0700

    integrating spotify api search functionality

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -3,29 +3,17 @@ import axios from "axios";
 
 import { GET_SPOTIFY_OBJECT, /*GET_USER_ID*/ GET_USER } from "./types";
 
-/*
 export const getSpotifyObject = spotify => async dispatch => {
   dispatch({ type: GET_SPOTIFY_OBJECT, payload: spotify });
 };
 
-export const getUserId = spotify => async dispatch => {
-  dispatch({ type: GET_USER_ID, payload: spotify});
-};
+// export const getUserId = spotify => async dispatch => {
+//   dispatch({ type: GET_USER_ID, payload: spotify});
+// };
 
-export const getUser = spotify => async dispatch => {
-  dispatch({ type: GET_USER, payload: spotify });
-};
-*/
-
-/* GET_SPOTIFY_OBJECT*/
-export function getSpotifyObject(api) {
-  const spotifyApi = new SpotifyWebApi();
-
-  return {
-    type: GET_SPOTIFY_OBJECT,
-    payload: spotifyApi
-  };
-}
+// export const getUser = spotify => async dispatch => {
+//   dispatch({ type: GET_USER, payload: spotify });
+// };
 
 /* GET_USER --> all the logic for User and UserID will occur outside of this?*/
 export function getUser(api) {
--- server/client/src/components/SearchBar.js
+++ server/client/src/components/SearchBar.js
@@ -1,48 +1,43 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import SpotifyWebApi from "spotify-web-api-js";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {term: ''};
+    this.state = { term: "" };
 
-    this.onInputChange = this.onInputChange.bind(this);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onInputChange = e => {
+      this.setState({ term: e.target.value });
+      console.log(e.target.value);
+    };
+
+    this.search = e => {
+      console.log(this.state.term);
+    };
   }
-
-  onInputChange(event) {
-    this.setState({ term: event.target.value });
-  }
-    
-  onFormSubmit(event) {
-    event.preventDefault();
-
-    console.log(this.props.spotifyApi);
-    /*.searchTracks(this.state.term)
-      .then(data => (console.log(data)))*/
-    }
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit} className="input-group">
+      <div>
         <input
           placeholder="Search for your favorite artists, albums, tracks, or playlists."
           className="form-control"
           value={this.state.term}
-          onChange={this.onInputChange} 
+          onChange={this.onInputChange}
         />
         <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">Submit</button>
+          <button onClick={this.search} className="btn btn-secondary">
+            Submit
+          </button>
         </span>
-      </form>
+      </div>
     );
-  };
-};
+  }
+}
 
 function mapStateToProps(state) {
   return {
-    spotifyApi: state.spotifyApi
+    spotify: state.spotify
   };
 }
 
